e3ea07ccabed236628feb3040b924aeec03e8618
ISSG2004 <ismaelsangon1234@gmail.com>
2024-01-30 11:47:47 +0100

    aaa

---
+++ 2Daw/EntornoCliente/Examenes/Recuperacion_RA4/script.js
@@ -0,0 +1,147 @@
+
+var Usuario = {
+    nombre: "",
+    contraseña: "",
+    Usuario:function(nombre,contraseña){
+        this.nombre=nombre;
+        this.contraseña=contraseña;
+    },
+    usuario:function(nombre,contraseña){//metodo constructor para crear el usuario
+        this.nombre=nombre;
+        this.contraseña=contraseña;
+    },
+    borrarContraseña:function(){//metodo para borrar la contraseña actual
+        this.contraseña="";
+    }
+};
+
+
+//instanciamos el objeto
+var usuarioCreado;
+//creamos ventanas
+var ventana1,ventana2,ventana3;
+//variable global para el vector
+var vectorEjercicio4=crearVector();
+//mostrar lista
+function mostrarLista(){
+    document.getElementById("valores").innerHTML=vectorEjercicio4;
+}
+//valores lista
+//valor lista1
+function extraerLista1(){
+    var valorLista1=parseInt(document.getElementById("lista1").value);
+    return valorLista1;
+}
+//valor lista2
+function extraerLista2(){
+    var valorLista2=parseInt(document.getElementById("lista2").value);
+    return valorLista2;
+}
+
+//funcionalidad ventana1
+
+function aVentana1(){
+    ventana1=window.open("_formUsuario.html","","height=300px,width=300px");
+    ventana1.moveTo(0,0);
+    
+}
+
+function verificarDatos(){
+    var usuarioIntroducido=document.getElementById("usuario").value;
+    var contraseñaIntroducida=parseInt(document.getElementById("contraseña").value);
+    var contraseñaRepetida=parseInt(document.getElementById("contraseñaRepetida").value);
+    if (contraseñaRepetida!=contraseñaIntroducida) {
+        document.getElementById("errorContraseña1").innerHTML="Las contraseñas introducidas son distinas";
+    } else {
+        if (usuarioIntroducido.length()>5 ||contraseñaIntroducida>9999) {
+            document.getElementById("errorContraseña1").innerHTML="Algun dato de usuario es incorrecto";
+        } else {
+            document.getElementById("aviso").innerHTML="Usuario creado correctamente";
+            usuarioCreado=new Usuario(usuarioIntroducido,contraseñaIntroducida);
+            document.getElementById("errorContraseña1").innerHTML="";
+            document.getElementById("aviso").innerHTML="Usuario creado correctamente";
+        }
+    }
+}
+
+
+//cerrar ventana1
+function cerrarV1(){
+    ventana1.close();
+}
+cerrarVentana1.addEventListener('click', cerrarV1);
+//funcionalidad ventana2
+function aVentana2(){
+    ventana2=window.open("_ventana2.html","","height=300px,width=300px");
+    ventana2.moveTo(1900,0);
+    ventana2.document.bgColor = 'blue';
+    setTimeout(function() {//asignar un cierre cuando pase x tiempo
+    ventana2.close();
+    }, 5000);//modificamos el tiempo en el que queremos que se cierre
+    
+}
+//mostrar datos usuario
+document.getElementById("usuarioV2").innerHTML=Usuario.nombre;
+document.getElementById("contraseañv2").innerHTML=Usuario.contraseña;
+function borrar(){//borramos contraseña
+    Usuario.borrarContraseña();
+}
+//cerrar ventana2
+function cerrarV2(){
+    ventana2.close();
+}
+cerrarVentana2.addEventListener('click', cerrarV2);
+//funcionalidad ventana3
+function aVentana3(){
+    ventana3=window.open("","","height=300px,width=300px");
+    ventana3.moveTo(800,1900);
+    ventana3.document.bgColor = 'pink';
+    ventana3.document.write("<p id='cronometro'></p>"+"numeros pares de la lista:"+pares());
+}
+//crear vector
+function crearVector(){
+    var lista=[];
+    for (let i = 0; i < extraerLista1(); i++) {
+        lista.push(parseInt(Math.random()*extraerLista2()));
+    }
+    return lista;
+}
+//seleccionar solo los pares
+function pares(){
+    var listaPares=[];
+    for (let i = 0; i < vectorEjercicio4.length; i++) {
+        if (vectorEjercicio4[i]%2==0) {
+            listaPares.push(parseInt(vectorEjercicio4[i]));
+        }
+    }
+    return listaPares;
+}
+//cuenta atrás
+var reloj;
+
+function inicio() {
+
+reloj=setInterval(procesar,1000);
+
+
+}
+
+function procesar() {
+
+var nro=extraerLista1();
+
+nro--;
+
+document.getElementById('cronometro').innerHTML=nro;
+
+}
+
+
+
+//cerrar ventana3
+function cerrarV3(){
+    ventana3.close();
+}
+
+cerrarVentana3.addEventListener('click', cerrarV3);
+
--- 2Daw/Estudio/script.js
+++ 2Daw/Estudio/script.js
@@ -2,15 +2,27 @@ var usuario = {
     nombreUsuario: "alumno",
     contraseña: "bueno",
     hora: "",
-    cambiarContraseña: function (nuevaContrtaseña) {
-        this.contraseña = nuevaContrtaseña;
-        this.asignarHora();
+    cambiarContraseña: function (nuevaContraseña) {
+        this.contraseña = nuevaContraseña;
+        this.hora = this.asignarHora();
     },
     asignarHora: function () {
-        let fechaActual = new Date();
-        this.hora = fechaActual.toLocaleString(); // Guardar la hora en formato legible
+        // Obtener la fecha actual
+        var fechaActual = new Date();
+        // Obtener los minutos y segundos
+        var minutos = fechaActual.getMinutes();
+        var segundos = fechaActual.getSeconds();
+        // Formatear los minutos y segundos para que siempre tengan dos dígitos
+        minutos = minutos < 10 ? '0' + minutos : minutos;
+        segundos = segundos < 10 ? '0' + segundos : segundos;
+        // Crear una cadena con la fecha, minutos y segundos
+        var fechaConMinutosYSegundos = fechaActual.toISOString().slice(0, 10) + ' ' + minutos + ':' + segundos;
+        // Almacenar la cadena en una variable
+        var resultado = fechaConMinutosYSegundos;
+        // Devolver el resultado
+        return resultado;
     }
-}
+};
 
 var ventana1;
 var ventana2;
@@ -52,10 +64,11 @@ cerrarv3.addEventListener('click', cerrarventana3);
 cerrarv4.addEventListener('click', cerrarventana4);
 
 // Funcionalidades de ventanas
+
 function abrirVentana1() {
     ventana1 = window.open('_ventanaForm.html', 'ventana1', 'height=300px,width=300px');
-    ventana1 = window.moveTo(0, 0);
-    ventana1.document.body.style.backgroundColor = "lightgreen";
+    ventana1.moveTo(0, 0);
+    //ventana1.document.body.style.backgroundColor = "lightgreen";
 }
 
 function validarUsuario() {
@@ -63,19 +76,23 @@ function validarUsuario() {
     let contrasenaIntroducida = document.getElementById("contraseña").value;
     let nuevaContraseña = document.getElementById("NuevaContraseña").value;
     if (usuarioIntroducido == usuario.nombreUsuario && contrasenaIntroducida == usuario.contraseña) {
-        alert("contraseña cambiada")
-        usuario.cambiarContraseña(nuevaContraseña);
+        let cambiarContraseña = confirm("Datos correctos, ¿Deseas cambiar la contraseña?");
+        if (cambiarContraseña) {
+            usuario.cambiarContraseña(nuevaContraseña);
+            alert("Contraseña cambiada. Nueva contraseña: " + nuevaContraseña);
+        }
     } else {
-        alert("Datos incorrectos ¿Quieres intentarlos de nuevo?");
+        alert("Datos incorrectos ¿Quieres intentarlo de nuevo?");
     }
 }
 
 // Cosas ventana2
+
 function abrirVentana2() {
     vectorGlobal = crearVector(); // Almacena el vector en la variable global
     ventana2 = window.open('', 'ventana2', 'height=300px,width=300px');
     ventana2.moveTo(1900, 0);
-    ventana2.document.write("listado de numeros" + vectorGlobal);
+    ventana2.document.write("Lista a representar: " + vectorGlobal.join('-'));
 }
 
 function generarNumeroAleatorio() {
@@ -83,7 +100,7 @@ function generarNumeroAleatorio() {
 }
 
 function crearVector() {
-    let vector=[];
+    let vector = [];
     for (let i = 0; i < generarLongitud(); i++) {
         vector.push(generarNumeroAleatorio());
     }
@@ -91,10 +108,12 @@ function crearVector() {
 }
 
 // Cosas ventana3
+
 function abrirVentana3() {
     ventana3 = window.open('', 'ventana3', 'height=300px,width=300px');
     ventana3.moveTo(1900, 1900);
-    ventana3.document.write("valores impares " + impares());
+    let imparesArray = impares();
+    ventana3.document.write("Valores impares: " + imparesArray.join('-'));
 }
 
 function impares() {
@@ -104,7 +123,8 @@ function impares() {
 
     return imparesArray;
 }
-//cosas ventana4
+
+// Cosas ventana4
 
 function abrirVentana4() {
     ventana4 = window.open('', 'ventana4', 'height=300px,width=300px');
@@ -112,8 +132,3 @@ function abrirVentana4() {
     ventana4.document.write("Contraseña vigente: " + usuario.contraseña + "<br>");
     ventana4.document.write("Última vez introducida: " + usuario.hora);
 }
-
-
-
-
-
