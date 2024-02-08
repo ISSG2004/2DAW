12e870f96045e90b2bcece755dfb62ad286c73d0
ISSG2004 <ismaelsangon1234@gmail.com>
2024-02-08 19:26:16 +0100

    igual pollo

--- 2Daw/EntornoServidor/Tema3/EJERCICIOS/MYSQLi/Ejercicio1.php
+++ 2Daw/EntornoServidor/Tema3/EJERCICIOS/MYSQLi/Ejercicio1.php
@@ -7,10 +7,16 @@
 </head>
 <body>
     <?php
-        $llamadaBD= new mysqli("localhost","dwes","abc123.","inmobiliaria");
-        #$llamadaBD->connect("localhost","dwes","abc123.","inmobiliaria");
-        $Titulo=$llamadaBD->query('SELECT Titulo FROM NOTICIAS');
-        
+        $llamadaBD=new mysqli("localhost","dwes","abc123.","inmobiliaria");
+        // Verificar si hay errores en la conexión
+        if ($llamadaBD->connect_error) {
+            die("Error en la conexión: " . $llamadaBD->connect_error);
+        }
+        $consulta=mysqli_query($llamadaBD,"SELECT * FROM noticias");
+        while ($registro = mysqli_fetch_array($consulta)) {
+            echo $registro['titulo'] . "<br>";
+        }
+        mysqli_close($llamadaBD);
     ?>
 </body>
 </html>
