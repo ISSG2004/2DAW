138d837f6114b847f73822a0149d546a6a323da9
ISSG2004 <ismaelsangon1234@gmail.com>
2024-02-12 18:12:45 +0100

    fin ejs php

--- 2Daw/EntornoServidor/Tema3/EJERCICIOS/MYSQLi/Ejercicio3.php
+++ 2Daw/EntornoServidor/Tema3/EJERCICIOS/MYSQLi/Ejercicio3.php
@@ -1,41 +1,64 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inmobiliaria - Eliminar</title>
 </head>
+
 <body>
     <?php
-        $llamadaBD=new mysqli("localhost","dwes","abc123.","inmobiliaria");
-        // Verificar si hay errores en la conexión
-        if ($llamadaBD->connect_errno==null) {
-            echo "<h1> Conexión correcta </h1>";     
-            $consulta=mysqli_query($llamadaBD,"SELECT * FROM noticias ORDER BY fecha DESC");
-            echo "<table>";
-            echo"<th>título</th>";
-            echo"<th>texto</th>";
-            echo"<th>categoria</th>";
-            echo"<th>fecha</th>";
-            echo "<th>Borrar</th>";
-            while ($registro = mysqli_fetch_array($consulta)) {
-                echo"<tr>";
-                echo "<td>".$registro['titulo']."</td>"."<td>".$registro['texto']."</td>"."<td>".$registro['categoria']."</td>"."<td>".$registro['fecha']."</td><td>"." <input type='checkbox' name='borrarNoticia[]' value='$registro['id']'>"."</td>" ;
-                echo "</tr>";
+    if (isset($_POST["eliminar"])) {
+        if (isset($_POST["ids"])) {
+            $ids = $_POST["ids"];
+            @$dwes = new mysqli('localhost', 'dwes', 'abc123.', 'inmobiliaria');
+            $error = $dwes->connect_errno;
+            if ($error == null) {
+                foreach ($ids as $id) {
+                    $eliminar = $dwes->stmt_init();
+                    $eliminar->prepare('DELETE FROM noticias WHERE id=?');
+                    $eliminar->bind_param('s', $id);
+                    $eliminar->execute();
+                    $eliminar->close();
+                }
+                $dwes->close();
+                echo "Se han eliminado las noticias: ";
+                foreach ($ids as $id) {
+                    echo "$id ";
+                }
+                echo "correctamente.";
             }
-            echo "</table>";
-            mysqli_close($llamadaBD);
-        }else {
-            echo "<h1>Conexión fallida </h1>";
         }
-        if (isset($_POST["borrarDatos"])) {
-            
-        } else {
-            
-    ?>
-    <input type="submit" name="borrarDatos" id="borrarDatos" value="Eliminar noticias marcadas">
-    <?php
     }
     ?>
+    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+        <h1>Eliminación de noticias</h1>
+        <table>
+            <tr>
+                <th>Título</th>
+                <th>Texto</th>
+                <th>Categoría</th>
+                <th>Fecha</th>
+                <th>Imagen</th>
+                <th>Borrar</th>
+            </tr>
+            <?php
+            @$dwes = new mysqli('localhost', 'dwes', 'abc123.', 'inmobiliaria');
+            $error = $dwes->connect_errno;
+            if ($error == null) {
+                $resultado = $dwes->query('SELECT titulo, texto, categoria, fecha, imagen, id FROM noticias ORDER BY fecha DESC');
+                $noticia = $resultado->fetch_object();
+                while ($noticia != null) {
+                    print "<tr><td>$noticia->titulo</td><td>$noticia->texto</td><td>$noticia->categoria</td><td>$noticia->fecha</td><td>$noticia->imagen</td><td><input type='checkbox' name='ids[]' value='$noticia->id'></td></tr>";
+                    $noticia = $resultado->fetch_object();
+                }
+                $dwes->close();
+            }
+            ?>
+        </table><br>
+        <input type="submit" name="eliminar" value="Eliminar noticias marcadas">
+    </form>
 </body>
+
 </html>
