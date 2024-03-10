7a01d283a9cd11aaa25297def0327b46446d8a96
ISSG2004 <ismaelsangon1234@gmail.com>
2024-03-10 21:37:43 +0100

    BBDD creada

--- 2Daw/EntornoServidor/ProyectoFinalDWE/bbdd.sql
+++ 2Daw/EntornoServidor/ProyectoFinalDWE/bbdd.sql
@@ -5,13 +5,30 @@ USE `pilotosF1`;
 -- --------------------------------------------------------
 --
 -- Estructura de tabla para la tabla `usuarios`
---
-CREATE TABLE IF NOT EXISTS `usuarios` (
-id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(20) NOT NULL,
-    contrasena VARCHAR(255) NOT NULL,
-    perfil ENUM('usuario', 'admin') NOT NULL
-);
-INE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+-- Establecer el motor de almacenamiento InnoDB
+SET default_storage_engine=INNODB;
+
+-- Crear la tabla de usuarios
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_usuario VARCHAR(50) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL,
+    tipo ENUM('admin', 'normal') NOT NULL
+) ENGINE=InnoDB;
+
+-- Crear la tabla de pilotos
+CREATE TABLE pilotos (
+    id INT PRIMARY KEY,
+    nombre_piloto VARCHAR(100) NOT NULL,
+    ruta_imagen VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id) REFERENCES usuarios(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+-- Agregar usuario administrador
+INSERT INTO usuarios (nombre_usuario, contrasena, tipo) VALUES ('admin', 'admin', 'admin');
+
+-- Agregar usuario normal
+INSERT INTO usuarios (nombre_usuario, contrasena, tipo) VALUES ('usuario', 'usuario', 'normal');
+
 
 GRANT ALL PRIVILEGES ON pilotosF1* TO 'dwes'@'localhost';
